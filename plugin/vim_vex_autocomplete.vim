7a3ee4ee105c112279109008b6d4de0a6e8c9993
Colin Kennedy <colinvfx@gmail.com>
2019-05-26 14:59:02 -0700

    WIP commit - getting expansion to work

--- plugin/vim_vex_autocomplete.vim
+++ plugin/vim_vex_autocomplete.vim
@@ -1,2 +1,19 @@
+function! ExpandVexSignature(completion)
+pythonx << EOF
+from vim_vex_autocomplete import autocomplete
+from UltiSnips import snippet_manager
+
+completion = vim.eval('a:completion')
+text = autocomplete.get_snippet(completion)
+
+if text:
+    autocomplete.clear_nearest_function()
+    snippet_manager.UltiSnips_Manager.expand_anon(text)
+EOF
+endfunction
+
+
+autocmd! CompleteDone *.vex :call ExpandVexSignature(v:completed_item)
+
 let s:current_directory = expand('<sfile>:p:h')
 execute ':set tags+=' . fnamemodify(s:current_directory, ':h') . '/ctags/vex-tags'
--- pythonx/vim_vex_autocomplete/autocomplete.py
+++ pythonx/vim_vex_autocomplete/autocomplete.py
@@ -11,6 +11,43 @@ import vim
 _FUNCTION_NAME = re.compile('(?P<name>\w+)\((?P<arguments>.+)\)')
 
 
+# Reference: https://stackoverflow.com/a/29992065/3626104
+def _find_parens(s):
+    toret = {}
+    pstack = []
+
+    for i, c in enumerate(s):
+        if c == '(':
+            pstack.append(i)
+        elif c == ')':
+            if len(pstack) == 0:
+                raise IndexError("No matching closing parens at: " + str(i))
+            toret[i] = pstack.pop()
+
+    if len(pstack) > 0:
+        raise IndexError("No matching opening parens at: " + str(pstack.pop()))
+
+    return toret
+
+
+def clear_nearest_function():
+    row, column = vim.current.window.cursor
+    row -= 1
+
+    parentheses = _find_parens(vim.current.buffer[row])
+    # If the completion is a function, the cursor position when the
+    # function is completed will always be a ). So we can query the
+    # matching ( using that column index
+    #
+    end = column - 1
+    start = parentheses[end]
+
+    vim.current.buffer[row] = vim.current.buffer[row][:start] \
+        + vim.current.buffer[row][end + 1:]
+
+    vim.current.window.cursor = (row + 1, start - 3)
+
+
 def get_argument_tabstops(arguments):
     # UltiSnips reservses the "0" tabstop for its own use. User tabstops start at 1
     offset = 1
@@ -19,7 +56,8 @@ def get_argument_tabstops(arguments):
 
 
 def get_completion_details(details):
-    if details.get('user_data', '') == '':
+    user_made_a_completion_selection = details.get('user_data', '') != ''
+    if not user_made_a_completion_selection:
         return ''
 
     return details['word']
@@ -31,10 +69,7 @@ def get_signature_breakdown(signature):
     if not match:
         return ''
 
-    name = match.group('name')
-    arguments = [argument.strip() for argument in match.group('arguments').split(',')]
-
-    return (name, arguments)
+    return [argument.strip() for argument in match.group('arguments').split(',')]
 
 
 def get_snippet(details):
@@ -43,7 +78,6 @@ def get_snippet(details):
     if not completion:
         return ''
 
-    function, arguments = get_signature_breakdown(completion)
+    arguments = get_signature_breakdown(completion)
     tabstops = get_argument_tabstops(arguments)
-
-    return '{function}({tabstops})'.format(function=function, tabstops=', '.join(tabstops))
+    return '({tabstops})'.format(tabstops=', '.join(tabstops))
